12bb40594d6841abceb098f15f2dba89bccf00ce
mprib <31831778+mprib@users.noreply.github.com>
2022-08-08 11:39:36 -0500

    more stuff to delete?

--- pose2sim/TestPose2Sim.py
+++
@@ -1,7 +0,0 @@
-# Mac, this is a file that you created
-
-from Pose2Sim import Pose2Sim
-import os
-
-Pose2Sim.triangulate3D(os.path.abspath(os.path.join("pose2sim", "Pose2Sim", "Demo","User", "Config.toml")))
-Pose2Sim.filter3D(os.path.abspath(os.path.join("pose2sim", "Pose2Sim", "Demo","User", "Config.toml")))
