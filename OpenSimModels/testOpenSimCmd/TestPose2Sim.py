f7b40305557d4333241a521097006a10281d9b3c
mprib <31831778+mprib@users.noreply.github.com>
2022-08-08 11:39:08 -0500

    deleting a bunch of things for simplicity..

---
+++ OpenSimModels/testOpenSimCmd/TestPose2Sim.py
@@ -0,0 +1,8 @@
+# Mac, this is a file that you created
+
+from Pose2Sim import Pose2Sim
+import os
+
+# Pose2Sim.triangulate3D(os.path.abspath(os.path.join("OpenSimModels", "testOpenSimCmd", "Config.toml")))
+
+Pose2Sim.filter3D(os.path.abspath(os.path.join("OpenSimModels", "testOpenSimCmd", "Config.toml")))
