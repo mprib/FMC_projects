c7aa02fa3468a15bedc6312b72a30ccd60d36d61
mprib <prible@gmail.com>
2022-08-13 08:06:41 -0500

    About to begin the process of converting array of trajectories produced by FMC into .trc that can be interpreted by OpenSim in the format of the model produced by pose2Sim

--- FMC_to_trc/create_blend.ps1
+++ FMC_to_trc/create_blend.ps1
@@ -1,4 +1,1 @@
-# This will create a .blend file from the FMC output 
-# The embedded code was running into problems in 
-
 & 'C:\Program Files\Blender Foundation\Blender 3.2\blender.exe' --background --python 'C:\Users\Mac Prible\anaconda3\envs\freemocap-env\Lib\site-packages\freemocap\freemocap_blender_megascript.py' -- 'C:\Users\Mac Prible\FreeMocap_Data\sesh_2022-08-10_10_33_12' 0
