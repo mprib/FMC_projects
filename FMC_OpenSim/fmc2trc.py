b3ade4451be5c75d1ab40f61357be1aa55842f15
mprib <prible@gmail.com>
2022-08-22 13:37:32 -0500

    start tracking working trc file to view changes

--- FMC_OpenSim/fmc2trc.py
+++ FMC_OpenSim/fmc2trc.py
@@ -16,27 +16,34 @@ import json
 class FMCSession():
     """Provide a session object to manage FMC output processing"""
 
-    def __init__(self, sessionID, FMC_folder, camera_rate):
+    def __init__(self, sessionID, FMC_folder, camera_rate, osim_file, pose_tracked="mediapipe_body_hands"):
             self.sessionID = sessionID
             self.camera_rate = camera_rate
             self.FMC_folder =  FMC_folder
+            self.osim_file = osim_file
+            self.pose_tracked = pose_tracked
 
-
-            # get a landmark list to reference
-            landmarks = [lm for lm in self.get_landmark_index().values()]            
-            # currently only care about the core pose and hands (first 75)
-            self.landmark_count = 75
-            self.landmarks = landmarks[0:self.landmark_count]
-
+            # tracked landmarks will be used to label trajectory array and 
+            # convert to human readable csv
+            self.tracked_landmarks = self.get_landmark_index(pose_tracked)
+            self.tracked_landmark_count = len(self.tracked_landmarks)
             self.trajectories = self.get_trajectory_dataframe()
+            
+            # modelled trajectories will be used to create the trc and ignore
+            # gap filling of non-modelled landmarks
+            #TODO this should get refactored to pull in the modelled 
+            # trajectories directly from the osim file
+            #pose_modeled = "mediapipe_body"
+            pose_modeled = "mediapipe_body_hands"
+            self.model_landmarks = self.get_landmark_index(pose_modeled)
 
-    def get_landmark_index(self):
+    def get_landmark_index(self, pose_key):
         """Read in a dictionary of the landmarks being tracked"""
         
-        with open("reference\mediapipe_landmarks.json") as f_obj:
+        with open("reference\landmarks.json") as f_obj:
             landmarks = json.load(f_obj)
 
-        return landmarks
+        return landmarks[pose_key]
 
 
     def get_trajectory_array(self):
@@ -70,22 +77,20 @@ class FMCSession():
         flip_y = axes_flip[1]
         flip_z = axes_flip[2]
 
-        landmark_trajectories = self.get_trajectory_array()
-
-
+        all_trajectories = self.get_trajectory_array()
 
         # not interested in face mesh or hands here, 
         # so only taking first 33 elements
         # these represent the gross pose + simple hands
-        lm_x = (landmark_trajectories[:, 0:self.landmark_count, x_axis] * flip_x * scale_factor)   # skeleton x data
-        lm_y = (landmark_trajectories[:, 0:self.landmark_count, y_axis] * flip_y * scale_factor)   # skeleton y data
-        lm_z = (landmark_trajectories[:, 0:self.landmark_count, z_axis] * flip_z * scale_factor)   # skeleton z data
+        lm_x = (all_trajectories[:, 0:self.tracked_landmark_count, x_axis] * flip_x * scale_factor)   # skeleton x data
+        lm_y = (all_trajectories[:, 0:self.tracked_landmark_count, y_axis] * flip_y * scale_factor)   # skeleton y data
+        lm_z = (all_trajectories[:, 0:self.tracked_landmark_count, z_axis] * flip_z * scale_factor)   # skeleton z data
         
 
         # convert landmark trajectory arrays to df and merge together
-        x_df = pd.DataFrame(lm_x, columns = [name + "_x" for name in self.landmarks])
-        y_df = pd.DataFrame(lm_y, columns = [name + "_y" for name in self.landmarks])
-        z_df = pd.DataFrame(lm_z, columns = [name + "_z" for name in self.landmarks])
+        x_df = pd.DataFrame(lm_x, columns = [name + "_x" for name in self.tracked_landmarks])
+        y_df = pd.DataFrame(lm_y, columns = [name + "_y" for name in self.tracked_landmarks])
+        z_df = pd.DataFrame(lm_z, columns = [name + "_z" for name in self.tracked_landmarks])
         merged_trajectories = pd.concat([x_df, y_df, z_df],axis = 1, join = "inner")    
 
 
@@ -95,7 +100,7 @@ class FMCSession():
 
         # get the correct order for all dataframe columns
         column_order = []
-        for marker in self.landmarks:
+        for marker in self.tracked_landmarks:
             column_order.append(marker + "_x")
             column_order.append(marker + "_y")
             column_order.append(marker + "_z")
@@ -109,9 +114,7 @@ class FMCSession():
 
         return merged_trajectories
 
-    def create_trajectory_csv(self, csv_filename):
-
-          
+    def create_trajectory_csv(self, csv_filename):          
         self.trajectories.to_csv(csv_filename, index=False)
 
 
@@ -127,12 +130,23 @@ class FMCSession():
 
         trajectories_for_trc = self.trajectories
 
+
+        # make a list of the trajectories to keep
+        keep_trajectories = []
+        for lm in self.model_landmarks:
+            keep_trajectories.append(lm+"_x")
+            keep_trajectories.append(lm+"_y")
+            keep_trajectories.append(lm+"_z")
+
+        print(keep_trajectories)
+
         # going to recreate these later after potentially dropping frames
-        original_column_order = trajectories_for_trc.columns
-        trajectories_for_trc = trajectories_for_trc.drop(columns=["Frame", "Time"]) 
-        # traj_df = traj_df.fillna("")
+        trajectories_for_trc = trajectories_for_trc[keep_trajectories]
+        
+
         trajectories_for_trc = trajectories_for_trc.dropna()
         
+
         orig_num_frames = len(trajectories_for_trc) - 1
         num_frames = orig_num_frames
 
@@ -164,16 +178,16 @@ class FMCSession():
 
             # create names of trajectories, skipping two columns (top of table)
             header_names = ['Frame#', 'Time']
-            for trajectory in self.landmarks:
+            for trajectory in self.model_landmarks:
                 header_names.append(trajectory)
                 header_names.append("")
                 header_names.append("")    
 
             tsv_writer.writerow(header_names)
 
-            # create labels for x,y,z axes (second row of table)
+            # create labels for x,y,z axes (below landmark names)
             header_names = ["",""]
-            for i in range(1,self.landmark_count):
+            for i in range(1,len(self.model_landmarks)+1):
                 header_names.append("X"+str(i))
                 header_names.append("Y"+str(i))
                 header_names.append("Z"+str(i))
@@ -207,16 +221,17 @@ class FMCSession():
         self.trajectories.interpolate(method='polynomial', order=3, inplace=True)
 
 
-
-
 GoodSession = "sesh_2022-08-10_10_33_12"
 FMC_folder = Path("C:/Users/Mac Prible/FreeMocap_Data")
+osim_file = "FMC_OpenSim\models\mediapipe_fullbody\mediapipe_fullbody_model.osim"
 
-testSession = FMCSession(GoodSession, FMC_folder, camera_rate=25)
+testSession = FMCSession(GoodSession, FMC_folder, osim_file=osim_file, camera_rate=25)
 
-trc_filename = "FMC_OpenSim/trc/dao_yin_dropped.trc" 
+trc_filename = "FMC_OpenSim/trc/dao_yin_dropped_working.trc" 
 testSession.create_trajectory_trc(trc_filename)
 
+
+testSession.create_trajectory_csv("FMC_OpenSim/trc/dao_yin.csv")
 testSession.interpolate_trajectory_gaps()
 
 trc_filename = "FMC_OpenSim/trc/dao_yin_interpolated.trc" 
