9b52e17bef5a9b961625f55d79a2005a02471089
mprib <prible@gmail.com>
2022-08-21 21:26:02 -0500

    continuing to clean up file structure

--- OpenSimModels/testOpenSimCmd/cmd_trc_IK.cmd
+++
@@ -1,1 +0,0 @@
-& 'C:\\OpenSim 4.4\\bin\\opensim-cmd.exe' run-tools IK_Setup_Pose2Sim_Body25b.xml
--- OpenSimModels/testOpenSimCmd/trc_InverseKinematics copy.ps1
+++
@@ -1,3 +0,0 @@
-& "C:\OpenSim 4.4\bin\opensim-cmd.exe" run-tool "C:\Users\Mac Prible\Box\Research\FMC_projects\OpenSimModels\testOpenSimCmd\Scaling_Setup_Pose2Sim_Body25b.xml"
-
-& "C:\OpenSim 4.4\bin\opensim-cmd.exe" run-tool "C:\Users\Mac Prible\Box\Research\FMC_projects\OpenSimModels\testOpenSimCmd\IK_Setup_Pose2Sim_Body25b.xml"
