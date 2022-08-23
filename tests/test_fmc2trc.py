042d96ab519634d6acfb0f8306ce063b865fa6db
mprib <prible@gmail.com>
2022-08-23 13:54:30 -0500

    able to run test in debug + CLI unittest discover

--- tests/test_fmc2trc.py
+++ tests/test_fmc2trc.py
@@ -1,8 +1,17 @@
 
+
 from pathlib import Path
+import sys
+repo = Path(__file__).parent.parent
+source = Path(repo, "FMC_OpenSim")
+sys.path.insert(0, str(source))
+
+# for p in sys.path:
+#     print(p)
+
 import unittest
 
-from FMC_OpenSim import fmc2trc
+from fmc2trc import FMCSession
 
 
 class TestFMC2trc(unittest.TestCase):
@@ -13,22 +22,22 @@ class TestFMC2trc(unittest.TestCase):
         Provided with the dao yin session and full body model,
         create a trc file for OpenSim
         """
-        FMC_folder = Path("FMC_OpenSim", "tests", "FMC_Sessions")
+        FMC_folder = Path( "tests", "FMC_Sessions")
         
         GoodSession = "sesh_2022-08-10_10_33_12"
 
-        osim_file = Path("FMC_OpenSim","models", "mediapipe_fullbody", "mediapipe_fullbody_model.osim")
+        osim_file = Path("models", "mediapipe_fullbody", "mediapipe_fullbody_model.osim")
 
-        testSession = fmc2trc.FMCSession(GoodSession, FMC_folder, osim_file=osim_file, camera_rate=25)
+        testSession = FMCSession(GoodSession, FMC_folder, osim_file=osim_file, camera_rate=25)
 
-        trc_filename = Path("FMC_OpenSim","tests", "output", "dao_yin_dropped_working.trc")
+        trc_filename = Path("tests", "output", "dao_yin_dropped_working.trc")
         testSession.create_trajectory_trc(trc_filename)
 
 
-        testSession.create_trajectory_csv(Path("FMC_OpenSim", "tests", "output", "dao_yin.csv"))
+        testSession.create_trajectory_csv(Path( "tests", "output", "dao_yin.csv"))
         testSession.interpolate_trajectory_gaps()
 
-        trc_filename = Path("FMC_OpenSim", "tests", "output", "dao_yin_interpolated.trc")
+        trc_filename = Path( "tests", "output", "dao_yin_interpolated.trc")
         testSession.create_trajectory_trc(trc_filename)
 
         
@@ -36,5 +45,5 @@ class TestFMC2trc(unittest.TestCase):
 
     # def test_model_inverse_kinematics(self):
    
-
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
