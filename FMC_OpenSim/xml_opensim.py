1d3c2e8c817a2a4980ef51c125541d0c67f0c1bc
mprib <prible@gmail.com>
2022-08-25 09:36:12 -0500

    about to refactor working csv export

--- FMC_OpenSim/xml_opensim.py
+++ FMC_OpenSim/xml_opensim.py
@@ -1,7 +1,8 @@
 
 # %%
 from pathlib import Path
-import xml.etree.ElementTree as ET
+# import xml.etree.ElementTree as ET
+import lxml.etree as etree
 import csv
 
 # %%
@@ -9,7 +10,7 @@ import csv
 repo = Path(__file__).parent.parent
 osim_path = Path(repo, "tests","osim_models", "mediapipe_fullbody_model.osim")
 
-osim_tree = ET.parse(osim_path)
+osim_tree = etree.parse(osim_path)
 
 osim_root = osim_tree.getroot()
 
@@ -33,35 +34,59 @@ for joint in osim_root.findall("Model/JointSet/objects/"):
         #     # print(translation == '0 0 0')
        
             # joint_csv.append([joint_name, physical_offset_frame, "'" + trans, socket_parent])
-# %%
 
 with open('model_joints.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerows(joint_csv)
 # %%
-# for child in osim_root[0]:
-#     if child.tag == "JointSet":
-#         for child2 in child[0]:
-#             #print("JointName: " + child2.attrib['name'])
-#             joint_name = child2.attrib['name']
-#             for child3 in child2:
-#                 if child3.tag == "frames":
-#                     for child4 in child3:
-#                         #print("    PhysicalOffsetFrame: " + child4.attrib['name'])
-#                         offset_frame = child4.attrib['name']
-#                         for child5 in child4:
-#                             if child5.tag == "translation":
-#                                 #print("        translation: " + child5.text)
-#                                 translation = child5.text
 
-#                             if child5.tag == "socket_parent":
-#                                 #print("        socket_parent: " + child5.text)
-#                                 socket_parent = child5.text
-#                         if translation != "0 0 0":
-#                             joint_csv.append([joint_name, offset_frame, "'" + translation, socket_parent])
+joint_csv = [["JointName", "PhysicalOffsetFrame", "Translation", "Socket_Parent"]]
 
+for child in osim_root[0]:
+    if child.tag == "JointSet":
+        for child2 in child[0]:
+            #print("JointName: " + child2.attrib['name'])
+            joint_name = child2.attrib['name']
+            for child3 in child2:
+                if child3.tag == "frames":
+                    for child4 in child3:
+                        #print("    PhysicalOffsetFrame: " + child4.attrib['name'])
+                        offset_frame = child4.attrib['name']
+                        for child5 in child4:
+                            if child5.tag == "translation":
+                                #print("        translation: " + child5.text)
+                                translation = child5.text
 
+                            if child5.tag == "socket_parent":
+                                #print("        socket_parent: " + child5.text)
+                                socket_parent = child5.text
+                        if translation != "0 0 0":
+                            joint_csv.append([joint_name, offset_frame, "'" + translation, socket_parent])
+
+with open('model_joints.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(joint_csv)   
 
 # %%
+joint_csv = [["JointName", "PhysicalOffsetFrame", "Translation", "Socket_Parent"]]
 
-import 
+
+for joint in osim_root.xpath('Model/JointSet/objects')[0]:
+    joint_name = joint.attrib['name']
+
+    for frame in joint.xpath("frames/PhysicalOffsetFrame"):
+        physical_offset_frame = name = frame.attrib['name']
+        translation = frame.xpath("translation")[0].text
+        socket_parent = frame.xpath("socket_parent")[0].text
+
+        if translation != "0 0 0":
+            joint_csv.append([joint_name, physical_offset_frame, "'" + translation, socket_parent])
+
+for line in joint_csv:
+    print(line)
+
+
+with open('model_joints.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(joint_csv)   
+# %%
